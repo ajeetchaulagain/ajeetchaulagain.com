aebe7363cd97303f95d6a2cb9997285626037b4d
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-02-04 19:44:35 +1100

    Additional CSS added. Call to action section in index page added

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,9 +7,13 @@ const AboutPage = () => {
     return (
             <Layout>
             <h1>About Me</h1>
-            <p>I am software engineer passionate about developing android applications lorem </p>
-            <p>lorem</p>
-            <p>Have a project idea? <Link to="/contact">Contact Me</Link></p> 
+            <p>I am software engineer passionate about developing android and web applications.Currently, I am working on <strong>full stack javascript development </strong>with react, mongo, node and express.</p>
+            <p>What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum h
+        as
+         been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also t</p>
+    
+         <p>Have a project idea? <Link to="/contact">Contact Me</Link></p> 
             </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,22 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Action from '../components/callltoaction'
+import { assertObjectType } from 'graphql'
+
+import image from '../images/ajeet.jpeg'
 
 const IndexPage = () => {
 
     return (
         <Layout>
+            <img src={image} width="100"/>
             <h1>Hello there.</h1>
-            <p>I am software engineer passionate about developing android and web applications.</p>
-            <p>Currently, I am working on <strong>full stack javascript development </strong>with react, mongo, node and express.</p>
-
-            <Action title="Say Hello to me" desc=" Need a developer for your project." url="contact" />
+            <p>I am software engineer passionate about developing android and web applications.Currently, I am working on <strong>full stack javascript development </strong>with react, mongo, node and express.</p>
+            <p>What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum h
+        as
+         been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also t</p>
+            
                
             
         </Layout>
