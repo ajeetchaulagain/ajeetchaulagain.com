903df4f9c9bcfd637cf4cc5dd941cfc734e0fa75
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-02-16 02:59:21 +1100

    Layout fixed

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,13 +4,44 @@ import MasterLayout from "../components/master-layout"
 import IntroSection from "../components/intro-section"
 import ContentLayout from "../components/content-layout"
 
+import { Link, graphql, useStaticQuery } from "gatsby"
+
 const Portfolio = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+            }
+            excerpt
+            html
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data);
+
+
+
   return (
     <MasterLayout>
       <IntroSection>Some of the featured projects of mine</IntroSection>
-
       <ContentLayout>
         <h1>Projects: List of all Projects</h1>
+        <ol>
+        {data.allMarkdownRemark.edges.map((edge)=>{
+              return(
+                <li>
+                  <h1>{edge.node.frontmatter.title}</h1>
+                </li>
+              )
+        })}
+        </ol>
+
         <Projects />
       </ContentLayout>
     </MasterLayout>
---
+++ src/projects/react-portfolio.md
@@ -0,0 +1,12 @@
+---
+title: "XML Keyword Search System"
+---
+
+A simple Java based keyword search system utilising various technologies such as
+Java XML, Java FX, Advance data structures such as HashMap, HashSet and ArrayList.
+
+## Skills I have learned from the project
+
+1. Java XML
+2. JavaFX
+3. Advance Data Structure
