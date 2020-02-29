828b45195a412d2753255a272d284cc988520afa
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-02-29 14:19:54 +1100

    Markup for the blog section added with component

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,14 +1,31 @@
 import React from "react"
-import blogStyles from "./blogs.module.scss"
+import blogStyles from "./blog.module.scss"
+
 
 const Blog = () => {
   return (
-    <div className={blogStyles.blog}>
-      <a href="#">How to Setup a CI/CD pipeline for MERN stack development</a>
-      <p>
-        In this article, I will explain how to setup a MERN stack development
-        with the CI/CD pipeline
-      </p>
+    <div className={blogStyles.blogWrapper}>
+      <article className={blogStyles.blogItem}>
+        <figure>
+          <img src= "./images/ajeet.jpg" />
+        </figure>
+
+        <div className = {blogStyles.content}>
+          <h2><a>This is a Title></a></h2>
+          <div className={blogStyles.metaData}>
+            <date>Published on 2014/12/12</date>
+            <span>Read Time: <strong>2min.</strong></span>
+            <ul>
+              <li> Nodejs</li>
+              <li>React JS</li>
+              <li>Gatsby</li>
+            </ul>
+          </div>
+
+          <p> This is the excerpt of a blog post</p>
+          <a>Read More Button</a>
+        </div>
+      </article>  
     </div>
   )
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,14 +2,14 @@ import React from "react"
 import MasterLayout from "../components/master-layout"
 import IntroSection from "../components/intro-section"
 import ContentLayout from "../components/content-layout"
-import Blogs from "../components/blogs"
+import Blog from "../components/blog"
 
 const BlogPage = () => {
   return (
     <MasterLayout>
       <IntroSection>Checkout my blog content</IntroSection>
       <ContentLayout>
-        <Blogs />
+        <Blog />
       </ContentLayout>
     </MasterLayout>
   )
