4712d83105cd7f2130dc318f4426bb7ad736d4c6
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-03-27 06:08:49 +1100

    customerly support added

--- src/html.js
+++ src/html.js
@@ -21,6 +21,24 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
+        
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.customerlySettings = {
+            app_id: "9328dcdb"
+        };
+        !function(){function e(){var e=t.createElement("script");
+        e.type="text/javascript",e.async=!0,
+        e.src="https://widget.customerly.io/widget/9328dcdb";
+        var r=t.getElementsByTagName("script")[0];r.parentNode.insertBefore(e,r)}
+        var r=window,t=document,n=function(){n.c(arguments)};
+        r.customerly_queue=[],n.c=function(e){r.customerly_queue.push(e)},
+        r.customerly=n,r.attachEvent?r.attachEvent("onload",e):r.addEventListener("load",e,!1)}();
+          `,
+        }}
+      />
+
       </body>
     </html>
   )
