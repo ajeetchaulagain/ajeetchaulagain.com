729b9c68b2d40ca9a0cd31fa3ab5d138c084c288
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-04-05 02:03:41 +1100

    manifest-file added

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,6 +130,21 @@ module.exports = {
         trackingId: "UA-54342929-2",
       },
     },
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/thumbnails/ajeet-avatar.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 
   pathPrefix: "/gatsby-portfolio",
