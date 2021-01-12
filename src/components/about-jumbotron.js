40c9befe4dc5985147b7eb3916e9e1e22071def8
ajeetchaulagain <chaulagainajeet@gmail.com>
2021-01-13 04:13:04 +1100

    update about

--- src/components/about-jumbotron.js
+++ src/components/about-jumbotron.js
@@ -17,9 +17,9 @@ const AboutJumbotronBlog = () => {
       </div>
       <div className={jumbotronStyles.contentWrapper}>
         <p>
-          I am Ajeet Chaulagain, a full stack developer based in Melbourne,
+          Hi, I am Ajeet, a pragmatic software engineer based in Melbourne,
           Australia. I write an article about
-          <em>modern software development and my side projects</em> to help the
+          <em> modern software development and my side projects</em> to help the
           beginner start their career in software engineering. If my site has
           helped you somehow to learn, I would be grateful if you consider
           supporting me.
