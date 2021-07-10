6c1620377b7061e745b735808789e0fe92397044
ajeetchaulagain <chaulagainajeet@gmail.com>
2021-07-10 23:25:20 +1000

    tyescript configuration added

--- src/components/utilities/buy-me-coffee-button.tsx
+++ src/components/utilities/buy-me-coffee-button.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import coffeImage from "../../images/cuplogo-sm.png"
 import * as buyMeCoffeeButtonStyles from "./buy-me-coffee-button.module.scss"
 
-const BuyMeCoffeeButton = () => {
+const BuyMeCoffeeButton = (): JSX.Element => {
   return (
     <div className={buyMeCoffeeButtonStyles.buymecoffeeWrapper}>
       <a href="https://ko-fi.com/ajeetchaulagain">
