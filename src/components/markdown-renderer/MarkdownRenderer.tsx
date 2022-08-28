0f1604a2002809e79eb925af6b1c84382e3f3782
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-08-29 00:05:33 +1000

    markdown bullet styling

--- src/components/markdown-renderer/MarkdownRenderer.tsx
+++ src/components/markdown-renderer/MarkdownRenderer.tsx
@@ -7,7 +7,11 @@ export const MarkdownRenderer = styled(MDXRenderer)`
     font-size: 1.1rem;
     list-style-type: disc;
     margin-left: 2rem;
+    &::marker {
+      color: #7b79f4;
+    }
   }
+
   ol li {
     margin-left: 1.1rem;
     margin-bottom: 0.6rem;
