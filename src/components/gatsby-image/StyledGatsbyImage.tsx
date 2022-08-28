641b261ad6379592df685d75483bf6eb245ab873
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-08-29 00:40:07 +1000

    add important falg

--- src/components/gatsby-image/StyledGatsbyImage.tsx
+++ src/components/gatsby-image/StyledGatsbyImage.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 
 export const StyledGatsbyImage = styled(GatsbyImage)`
   & > picture > img {
-    opacity: 1;
+    opacity: 1 !important; // Fixes for opacity injection issue in gatbsy-plugin-image
   }
 `;
