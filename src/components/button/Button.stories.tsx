82b6ee306903b4948e41d24444ef7347e0fda456
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-12-25 23:37:44 +1100

    wip add storybook

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { PrimaryButtonLink } from './Button';
+
+export default {
+  component: PrimaryButtonLink,
+  title: 'Components/Button',
+};
+
+const Template = (args) => <PrimaryButtonLink>Hello</PrimaryButtonLink>;
+
+export const Default = Template.bind({});
