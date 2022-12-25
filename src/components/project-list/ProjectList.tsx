b4b4d046c2026ec27292fd540109579696956789
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-12-26 00:10:09 +1100

    cleanup ProjectList.tsx

--- src/components/project-list/ProjectList.tsx
+++ src/components/project-list/ProjectList.tsx
@@ -12,20 +12,22 @@ export const ProjectList = ({ projectList }: ProjectListProps): JSX.Element => {
   return (
     <div className={projectStyles.projects}>
       {projectList.map((edge) => {
+        const {
+          node: {
+            fields: { slug },
+            frontmatter,
+          },
+        } = edge;
+
         return (
-          <Link
-            to={`/projects/${edge.node.fields.slug}`}
-            className={projectStyles.projectItem}
-          >
+          <Link to={`/projects/${slug}`} className={projectStyles.projectItem}>
             <div className={projectStyles.contentWrapper}>
               <h2>{edge.node.frontmatter.title}</h2>
 
               <ul className={projectStyles.toolsWrapper}>
-                {edge.node.frontmatter.technologies.map(
-                  (technology: string) => {
-                    return <li>{technology}</li>;
-                  }
-                )}
+                {frontmatter.technologies.map((technology) => {
+                  return <li>{technology}</li>;
+                })}
               </ul>
             </div>
             {true && (
