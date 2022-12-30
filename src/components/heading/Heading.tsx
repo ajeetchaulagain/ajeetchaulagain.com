a99a2e5677d9f09e896835f6cab32e0049194aa3
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-12-30 17:18:37 +1100

    cleanup heading component

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -1,42 +1,8 @@
 import { HeadingFontSize } from 'components/theme/Theme';
 import React, { ReactNode } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 //TODO: Revisit this file to remove !important once overriding scss is cleaned up
-
-const Heading1 = styled.h1<{ size?: HeadingFontSize; type?: FontType }>`
-  font-size: ${({ size, theme: { headingFontSizes } }) =>
-    size ? headingFontSizes[size] : headingFontSizes.xlarge} !important;
-
-  font-family: ${({ type, theme: { fonts } }) =>
-    type === 'sans-serif'
-      ? fonts.secondaryHeading
-      : fonts.primaryHeading} !important;
-  font-weight: ${(props) => props.theme.fontWeights[9]};
-`;
-
-const Heading2 = styled.h2<{ size?: HeadingFontSize; type?: FontType }>`
-  font-size: ${({ size, theme: { headingFontSizes } }) =>
-    size ? headingFontSizes[size] : headingFontSizes.xlarge} !important;
-
-  font-family: ${({ type, theme: { fonts } }) =>
-    type === 'sans-serif'
-      ? fonts.secondaryHeading
-      : fonts.primaryHeading} !important;
-  font-weight: ${(props) => props.theme.fontWeights[6]};
-`;
-
-const Heading3 = styled.h3<{ size?: HeadingFontSize; type?: FontType }>`
-  font-size: ${({ size, theme: { headingFontSizes } }) =>
-    size ? headingFontSizes[size] : headingFontSizes.large} !important;
-
-  font-family: ${({ type, theme: { fonts } }) =>
-    type === 'sans-serif'
-      ? fonts.secondaryHeading
-      : fonts.primaryHeading} !important;
-
-  font-weight: ${(props) => props.theme.fontWeights[5]};
-`;
 
 type FontType = 'serif' | 'sans-serif';
 
@@ -46,6 +12,27 @@ type HeadingProps = {
   level: 'h1' | 'h2' | 'h3';
   children: ReactNode;
 };
+
+const Heading1 = styled.h1<{ size?: HeadingFontSize; type?: FontType }>`
+  font-size: ${({ size, theme: { headingFontSizes } }) =>
+    size ? headingFontSizes[size] : headingFontSizes.xlarge};
+
+  font-weight: ${(props) => props.theme.fontWeights[9]};
+`;
+
+const Heading2 = styled.h2<{ size?: HeadingFontSize; type?: FontType }>`
+  font-size: ${({ size, theme: { headingFontSizes } }) =>
+    size ? headingFontSizes[size] : headingFontSizes.xlarge};
+
+  font-weight: ${({ theme }) => theme.fontWeights[6]};
+`;
+
+const Heading3 = styled.h3<{ size?: HeadingFontSize; type?: FontType }>`
+  font-size: ${({ size, theme: { headingFontSizes } }) =>
+    size ? headingFontSizes[size] : headingFontSizes.large};
+
+  font-weight: ${({ theme }) => theme.fontWeights[5]};
+`;
 
 export const Heading = ({ level, children, ...rest }: HeadingProps) => {
   let HeadingComponent;
@@ -64,7 +51,13 @@ export const Heading = ({ level, children, ...rest }: HeadingProps) => {
       break;
   }
 
-  return <HeadingComponent {...rest}>{children}</HeadingComponent>;
+  const StyledHeadingComponent = styled(HeadingComponent)`
+    font-family: ${({ type, theme: { fonts } }) =>
+      type === 'sans-serif' ? fonts.secondaryHeading : fonts.primaryHeading};
+    line-height: 1.5;
+  `;
+
+  return <StyledHeadingComponent {...rest}>{children}</StyledHeadingComponent>;
 };
 
 Heading.defaultProps = {
