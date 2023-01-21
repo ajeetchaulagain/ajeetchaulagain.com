c8728162b754e507714c9ed08f5fac45a238fd20
ajeetchaulgain <chaulagainajeet@gmail.com>
2023-01-21 21:19:06 +1100

    change font weight

--- src/components/info-call-to-action/InfoCallToAction.tsx
+++ src/components/info-call-to-action/InfoCallToAction.tsx
@@ -17,7 +17,7 @@ const Container = styled.div`
     font-family: ${({ theme }) => theme.fonts.body};
     color: ${({ theme }) => theme.colors.brandPrimary};
     text-decoration: underline;
-    font-weight: ${({ theme }) => theme.fontWeights[5]};
+    font-weight: ${({ theme }) => theme.fontWeights[6]};
     :hover {
       text-decoration: none;
     }
@@ -28,7 +28,7 @@ const StyledParagraph = styled(Paragraph)`
   ${mb(0)};
   line-height: 1.5;
   font-size: ${({ theme }) => theme.fontSizes.small};
-  font-weight: ${({ theme }) => theme.fontWeights[3]};
+  font-weight: ${({ theme }) => theme.fontWeights[4]};
 `;
 
 const IconWrapper = styled.div`
