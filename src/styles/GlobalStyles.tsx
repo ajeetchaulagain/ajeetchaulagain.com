a825329618783c6af454384e9bede43973234ca0
ajeetchaulgain <chaulagainajeet@gmail.com>
2023-01-29 14:48:47 +1100

    add google fonts as global style - styled-components

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -3,10 +3,12 @@ import { CSSReset } from './CSSReset';
 import { BaseStyles } from './BaseStyles';
 import { MarkdownTableStyles } from './MarkdownTableStyles';
 import { CodeBlockStyles } from './CodeBlockStyles';
+import * as GoogleFonts from './GoogleFonts';
 
 export const GlobalStyle = createGlobalStyle`
 ${CSSReset};
 ${BaseStyles};
 ${MarkdownTableStyles};
 ${CodeBlockStyles};
+${GoogleFonts};
 `;
