c38f75f5454b4939d156e1cd2776785a19cf58a3
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2026-01-26 20:12:22 +1100

    Add playwright test for visual regression

---
+++ tests/example.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('HomePage Visual Regression', async ({ page }) => {
+  await page.goto('https://ajeetchaulagain.com/');
+  // Compare full page screenshot against baseline
+  await expect(page).toHaveScreenshot('homepage.png', {
+    fullPage: true,
+    maxDiffPixels: 100, // Allow small variations (optional)
+  });
+});
+
+
+
+test('Blog Listing page Visual Regression', async ({ page }) => {
+  await page.goto('https://ajeetchaulagain.com/blog');
+  await expect(page).toHaveScreenshot('blog-listing-page.png', { fullPage: true });
+});
+
+test('Project Listing page Regression', async ({ page }) => {
+  await page.goto('https://ajeetchaulagain.com/projects');
+  await expect(page).toHaveScreenshot('project-listing-page.png', { fullPage: true });
+});
+
+
