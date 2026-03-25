5d8ddd74dfbf552bcf47575e99e818e2299c279e
Ajeet Chaulagain <chaulagainajeet@gmail.com>
2026-03-25 11:18:23 +1100

    Add a tooltip for social media icons

--- src/components/social-media-icons/styles.tsx
+++ src/components/social-media-icons/styles.tsx
@@ -3,7 +3,30 @@ import { ml } from 'styled-components-spacing';
 import breakpoint from 'styled-components-breakpoint';
 import { Link } from '../link/Link';
 
-export const ListItem = styled.li``;
+export const Tooltip = styled.span`
+  position: absolute;
+  bottom: calc(100% + 6px);
+  left: 50%;
+  transform: translateX(-50%);
+  background: ${({ theme }) => theme.colors.brandPrimary};
+  color: ${({ theme }) => theme.colors.primaryBackground};
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: 11px;
+  white-space: nowrap;
+  padding: 3px 8px;
+  border-radius: 4px;
+  pointer-events: none;
+  opacity: 0;
+  transition: opacity 0.15s ease;
+`;
+
+export const ListItem = styled.li`
+  position: relative;
+
+  &:hover ${Tooltip} {
+    opacity: 1;
+  }
+`;
 
 export const SocialMediaList = styled.ul`
   display: flex;
